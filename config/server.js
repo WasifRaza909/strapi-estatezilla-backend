3a0d9ae64316b84f561e798ed33fbac10c7a1711
unknown <wasifraza909@gmail.com>
2021-06-14 19:13:20 +0500

    Installed Strapi CLI as well as attached cloudinary with it

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c3123497386304b68919480452d9c40e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7e363c84-dc25-47e1-b700-5921dc274717'
+};
